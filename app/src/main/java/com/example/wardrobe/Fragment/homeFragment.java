128c48981a9f4aedae0eae134f95a06b322b617f
999dengA <orcaandreaannep@gmail.com>
2023-04-04 18:32:27 +0800

    updated UI

--- app/src/main/java/com/example/wardrobe/Fragment/homeFragment.java
+++ app/src/main/java/com/example/wardrobe/Fragment/homeFragment.java
@@ -1,17 +1,31 @@
 package com.example.wardrobe.Fragment;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import com.bumptech.glide.Glide;
+import com.example.wardrobe.Model.User;
 import com.example.wardrobe.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class homeFragment extends Fragment {
 
-    ImageButton profileBtn;
+    CircleImageView image_profile;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,10 +33,26 @@ public class homeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        profileBtn = view.findViewById(R.id.image_profile);
+        image_profile = view.findViewById(R.id.image_profile);
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(Objects.requireNonNull(firebaseUser).getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                Glide.with(requireContext()).load(Objects.requireNonNull(user).getImageurl()).into(image_profile);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         // directs in profile section
-        profileBtn.setOnClickListener(v -> {
+        image_profile.setOnClickListener(v -> {
             profileFragment profileFragment = new profileFragment();
             requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, profileFragment).commit();
         });
