c757cb7862fcc850851764eee587bef9e575f807
AndreaOrca <202110209@gordoncollege.edu.ph>
2023-05-31 17:32:48 +0800

    wardrobe categories

--- app/src/main/java/com/example/wardrobe/Fragment/WardrobeFragment.java
+++ app/src/main/java/com/example/wardrobe/Fragment/WardrobeFragment.java
@@ -9,13 +9,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.wardrobe.CasualActivity;
 import com.example.wardrobe.FormalActivity;
 import com.example.wardrobe.R;
 
 
 public class WardrobeFragment extends Fragment {
 
-    Button formalBtn;
+    Button formalBtn, casualBtn, activeBtn, businessBtn, eveningBtn, othersBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,8 +25,14 @@ public class WardrobeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_wardrobe, container, false);
 
         formalBtn = view.findViewById(R.id.formalBtn);
+        casualBtn = view.findViewById(R.id.casualBtn);
+        activeBtn = view.findViewById(R.id.activeBtn);
+        businessBtn = view.findViewById(R.id.businessBtn);
+        eveningBtn = view.findViewById(R.id.eveningBtn);
+        othersBtn = view.findViewById(R.id.othersBtn);
 
         formalBtn.setOnClickListener(view1 -> startActivity(new Intent(getContext(), FormalActivity.class)));
+        casualBtn.setOnClickListener(view1 -> startActivity(new Intent(getContext(), CasualActivity.class)));
 
         return view;
     }
--- app/src/main/java/com/example/wardrobe/Model/Gallery.java
+++ app/src/main/java/com/example/wardrobe/Model/Gallery.java
@@ -2,18 +2,26 @@ package com.example.wardrobe.Model;
 
 public class Gallery {
     private String imageUrl;
+    private String userId;
+
+    public Gallery(String imageUrl, String userId) {
+        this.imageUrl = imageUrl;
+        this.userId = userId;
+    }
+
     public Gallery() {
 
-    }
-    public Gallery(String imageUrl) {
-        this.imageUrl = imageUrl;
     }
 
     public String getImageUrl() {
         return imageUrl;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 }
